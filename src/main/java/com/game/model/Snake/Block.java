8b98485ebb7d4e36da151ad41180ddac56ba2866
A-Mundanilkunathil <aaronchackom2002@gmail.com>
2024-11-13 22:53:32 -0800

    Completed All basic functionality for Snake Game

--- src/main/java/com/game/model/Snake/Block.java
+++ src/main/java/com/game/model/Snake/Block.java
@@ -7,13 +7,15 @@ import javafx.scene.shape.Rectangle;
 public class Block extends Rectangle{
     int posX, posY, oldPosX, oldPosY;
 
-    static final int UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
+    public static final int UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
 
     Block previous;
 
+    
     int direction = LEFT;
+    int maxX, maxY;
 
-    public Block(int x, int y, Block p){
+    public Block(int x, int y, Block p , Field f){
         super(SnakeUI.block_size, SnakeUI.block_size);
         posX = x;
         posY = y; 
@@ -22,11 +24,13 @@ public class Block extends Rectangle{
         setTranslateY(posY * SnakeUI.block_size);
 
         previous = p;
+        maxX = f.getW();
+        maxY = f.getH();
+
     }
 
 
     public void update(){
-        
         oldPosX = posX;
         oldPosY = posY;
         if (previous == null){
@@ -44,26 +48,45 @@ public class Block extends Rectangle{
                     moveLeft();
                     break;
             }
-
-
         }
         else { 
             posX = previous.oldPosX;
             posY = previous.oldPosY;
 
         }
+        updatePosition();
     }
 
     public void moveUp(){
         posY--;
+        if(posY < 0) { 
+            posY = maxY - 1;
+
+        }
     }
     public void moveDown(){
         posY++;
+        if ( posY >= maxY) { 
+            posY = 0;
+        }
     }
     public void moveLeft(){
         posX--;
+        if (posX < 0){
+            posX = maxX - 1;
+
+        }
     }
     public void moveRight(){
         posX++;
+        if (posX >= maxX){
+            posX = 0;
+        }
+    }
+
+    public void updatePosition(){
+        setTranslateX(posX * SnakeUI.block_size);
+        setTranslateY(posY * SnakeUI.block_size);
+
     }
 }
--- src/main/java/com/game/ui/SnakeUI.java
+++ src/main/java/com/game/ui/SnakeUI.java
@@ -1,14 +1,24 @@
 package com.game.ui;
 
+import com.game.model.Snake.Block;
 import com.game.model.Snake.Field;
 import com.game.model.Snake.Snake;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+//TODO : mport for font
+//TODO : import for label
 
 
 
@@ -19,25 +29,84 @@ public class SnakeUI extends Application {
     int width = 30, height = 15;
     int il = 5;
 
+    long then = System.nanoTime();
+
+    boolean changed = false;
+    int nextUpdate;
+    boolean hasNext = false;
+
+    Field f;
+
     @Override
     public void start(Stage ps) {
         VBox gameVBox = new VBox(10);
         gameVBox.setPadding(new Insets(10));
 
-        Field f = new Field(width, height);
+        f = new Field(width, height);
         f.addSnake(new Snake(il,f));
 
+        Label score = new Label("Score : 0");
+        score.setFont(Font.font("tahoma", 32));
+        //speed control for the snake
         AnimationTimer timer = new AnimationTimer(){
             public void handle(long now){
-            f.update();
+                if (now - then > 100000000/2){
+                        f.update();
+                        then = now;
+                        score.setText("Score: "+f.score);
+                        changed = false;
+                        if (hasNext) { 
+                                setDirection(f.snake, nextUpdate);
+                                hasNext = false;
+                        }
+
+                        if (f.isDead()){
+                                stop();
+                                Alert al = new Alert(AlertType.INFORMATION);
+                                al.setHeaderText("YOUR SNAKE DIED");
+                                al.setContentText("Score reached : "+f.score);
+                                al.showAndWait();
+                                Platform.runLater(al::showAndWait);
+
+                        al.setOnHidden(e->{
+                                gameVBox.getChildren().clear();
+                                f = new Field(width, height);
+                                f.addSnake(new Snake(il, f));
+                                score.setText("Score: 0");
+                                gameVBox.getChildren().addAll(f,score);
+                                start();
+                        });
+                }
+        }
             }
         };
         timer.start();
 
-        gameVBox.getChildren().add(f);
+        gameVBox.getChildren().addAll(f, score);
 
         Scene scene = new Scene(gameVBox);
 
+        
+        scene.setOnKeyPressed(e -> { 
+                if(e.getCode().equals(KeyCode.UP) && f.snake.getDirection() != Block.DOWN){
+                        f.snake.setDirection(Block.UP);
+
+                }
+                if(e.getCode().equals(KeyCode.DOWN) && f.snake.getDirection() != Block.UP){
+                        f.snake.setDirection(Block.DOWN);
+
+                }
+                if(e.getCode().equals(KeyCode.RIGHT) && f.snake.getDirection() != Block.LEFT){
+                        f.snake.setDirection(Block.RIGHT);
+
+                }
+                if(e.getCode().equals(KeyCode.LEFT) && f.snake.getDirection() != Block.RIGHT){
+                        f.snake.setDirection(Block.LEFT);
+
+                }
+
+
+        });
         ps.setResizable(false);
         ps.setScene(scene);
 
@@ -45,6 +114,19 @@ public class SnakeUI extends Application {
 
         ps.show();
 
+   
+
     }
+
+    public void setDirection(Snake s, int d){ 
+        if(!changed){
+                s.setDirection(d);
+                changed = true;
+        }
+        else { 
+                hasNext = true;
+                nextUpdate = d;
+        }
+        }
 
 }
